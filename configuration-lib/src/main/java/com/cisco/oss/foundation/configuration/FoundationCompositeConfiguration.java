d213ec2a66465af05a555d7a4bbddb892f77d215
Yair Ogen <yaogen@cisco.com>
2015-03-31 17:41:31 +0300

    add override of getBoolean to perorm better

--- configuration-lib/src/main/java/com/cisco/oss/foundation/configuration/FoundationCompositeConfiguration.java
+++ configuration-lib/src/main/java/com/cisco/oss/foundation/configuration/FoundationCompositeConfiguration.java
@@ -28,41 +28,41 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * this is an extension of the commons configuration CompositeConfiguration to
  * enable sorting the keys before returning them to the users.
- * 
+ *
  * @author Yair Ogen
  */
 public class FoundationCompositeConfiguration extends CompositeConfiguration {
 
-	private final Map<String, Object> cache = new ConcurrentHashMap<String, Object>();
-	private static final boolean DISABLE_CACHE = Boolean.getBoolean("configuration.disableCache");
+    private final Map<String, Object> cache = new ConcurrentHashMap<String, Object>();
+    private static final boolean DISABLE_CACHE = Boolean.getBoolean("configuration.disableCache");
 
-	/**
-	 * each call to this method method will first sort the keys and then return
-	 * the iterator.
-	 */
-	@Override
-	@SuppressWarnings("unchecked")
-	/**
-	 * override to support ordering of returned keys.
-	 */
-	public Iterator<String> getKeys() {
+    /**
+     * each call to this method method will first sort the keys and then return
+     * the iterator.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    /**
+     * override to support ordering of returned keys.
+     */
+    public Iterator<String> getKeys() {
 
-		final Iterator tempKeysIter = super.getKeys();
-		final List<String> keys = sortKeys(tempKeysIter);
+        final Iterator tempKeysIter = super.getKeys();
+        final List<String> keys = sortKeys(tempKeysIter);
 
-		return keys.iterator();
-	}
+        return keys.iterator();
+    }
 
-	private List<String> sortKeys(final Iterator<String> tempKeysIter) {
-		final List<String> keys = new ArrayList<String>();
-		while (tempKeysIter.hasNext()) {
-			final String key = (String) tempKeysIter.next();
-			keys.add(key);
-		}
+    private List<String> sortKeys(final Iterator<String> tempKeysIter) {
+        final List<String> keys = new ArrayList<String>();
+        while (tempKeysIter.hasNext()) {
+            final String key = (String) tempKeysIter.next();
+            keys.add(key);
+        }
 
-		Collections.sort(keys);
-		return keys;
-	}
+        Collections.sort(keys);
+        return keys;
+    }
 
     public boolean containsKeyFromCommonsConfig(String key) {
         return super.containsKey(key);
@@ -77,30 +77,30 @@ public class FoundationCompositeConfiguration extends CompositeConfiguration {
         }
     }
 
-	@Override
-	public Object getProperty(String key) {
-		if (DISABLE_CACHE) {
-			return super.getProperty(key);			
-		} else {
-			if (cache.containsKey(key)) {
-				return cache.get(key);
-			} else {
-				Object value = super.getProperty(key);
-				if (value != null)
-					cache.put(key, value);
-				return value;
-			}
-		}
-	}
+    @Override
+    public Object getProperty(String key) {
+        if (DISABLE_CACHE) {
+            return super.getProperty(key);
+        } else {
+            if (cache.containsKey(key)) {
+                return cache.get(key);
+            } else {
+                Object value = super.getProperty(key);
+                if (value != null)
+                    cache.put(key, value);
+                return value;
+            }
+        }
+    }
 
-	public void clearCache() {
-		cache.clear();
-	}
+    public void clearCache() {
+        cache.clear();
+    }
 
-	public void updateCache(String key, Object value) {
+    public void updateCache(String key, Object value) {
         if (value == null) {
             cache.remove(key);
-        }else{
+        } else {
             cache.put(key, value);
         }
     }
@@ -111,31 +111,28 @@ public class FoundationCompositeConfiguration extends CompositeConfiguration {
         updateCache(key, value);
     }
 
-	@Override
-	public boolean getBoolean(String key) {
-		Boolean b = getBoolean(key, null);
-		if (b != null)
-		{
-			return b.booleanValue();
-		}
-		else
-		{
-			throw new NoSuchElementException('\'' + key + "' doesn't map to an existing object");
-		}
-	}
+    @Override
+    public boolean getBoolean(String key) {
+        Boolean b = getBoolean(key, null);
+        if (b != null) {
+            return b.booleanValue();
+        } else {
+            throw new NoSuchElementException('\'' + key + "' doesn't map to an existing object");
+        }
+    }
 
-	@Override
-	public boolean getBoolean(String key, boolean defaultValue) {
-		return getBoolean(key, (Boolean)defaultValue);
-	}
+    @Override
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return getBoolean(key, (Boolean) defaultValue);
+    }
 
-	@Override
-	public Boolean getBoolean(String key, Boolean defaultValue) {
-		Object property = getProperty(key);
-		if (property != null) {
-			return BooleanUtils.toBooleanObject(property.toString());
-		} else {
-			return defaultValue;
-		}
-	}
+    @Override
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        Object property = getProperty(key);
+        if (property != null) {
+            return BooleanUtils.toBooleanObject(property.toString());
+        } else {
+            return defaultValue;
+        }
+    }
 }
